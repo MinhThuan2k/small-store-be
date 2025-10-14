1c2606d2b1737ffb5bb5e712e7951041c92253c8
thuanle <thuanle96.tl@gmail.com>
2025-10-14 14:44:32 +0700

    fix error response

--- src/exception/ExceptionHandle.ts
+++ src/exception/ExceptionHandle.ts
@@ -14,46 +14,50 @@ export class ExceptionHandle implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const request = ctx.getRequest();
     const response = ctx.getResponse<FastifyReply>();
+    try {
+      const status =
+        exception instanceof HttpException ? exception.getStatus() : 500;
+      const exceptionResponse =
+        exception instanceof HttpException
+          ? {
+              message: (exception.getResponse() as any).message,
+              statusCode: status,
+            }
+          : {
+              message: exception.message || 'Internal server error',
+              statusCode: status,
+            };
 
-    const status =
-      exception instanceof HttpException ? exception.getStatus() : 500;
-    const exceptionResponse =
-      exception instanceof HttpException
-        ? {
-            message: (exception.getResponse() as any).message,
-            statusCode: status,
-          }
-        : {
-            message: exception.message || 'Internal server error',
-            statusCode: status,
-          };
+      // log
+      if (process.env.VERCEL !== '1') {
+        const logsDir = path.join(__dirname, '../../logs');
+        const logFileName = `logs-${new Date().toISOString().split('T')[0]}.log`;
+        const logFilePath = path.join(logsDir, logFileName);
+        await fs.ensureDir(logsDir);
+        await fs.appendFile(
+          logFilePath,
+          `[${new Date().toISOString()}]\t${JSON.stringify({
+            url: request.url,
+            method: request.method,
+            body: request.body,
+            headers: request.headers,
+          })}\n` + `\tstack: ${exception.stack} \n`,
+        );
+      }
 
-    // log
-    if (process.env.VERCEL !== '1') {
-      const logsDir = path.join(__dirname, '../../logs');
-      const logFileName = `logs-${new Date().toISOString().split('T')[0]}.log`;
-      const logFilePath = path.join(logsDir, logFileName);
-      await fs.ensureDir(logsDir);
-      await fs.appendFile(
-        logFilePath,
-        `[${new Date().toISOString()}]\t${JSON.stringify({
-          url: request.url,
-          method: request.method,
-          body: request.body,
-          headers: request.headers,
-        })}\n` +
-          `\terror: ${JSON.stringify(exception.getResponse().errors || {})} \n` +
-          `\tstack: ${exception.stack} \n`,
-      );
+      // trả response
+      response.status(status).send({
+        message: exceptionResponse.message,
+        name: exception.name || 'Error',
+        stack:
+          process.env.NODE_ENV === 'development' ? exception?.stack : undefined,
+      });
+    } catch (e: any) {
+      response.status(500).send({
+        message: 'Internal server error',
+        name: 'Error',
+        stack: process.env.NODE_ENV === 'development' ? e?.stack : undefined,
+      });
     }
-
-    // trả response
-    response.status(status).send({
-      message: exceptionResponse.message,
-      name: exception.name || 'Error',
-      error: exception.getResponse().errors || null,
-      stack:
-        process.env.NODE_ENV === 'development' ? exception?.stack : undefined,
-    });
   }
 }
